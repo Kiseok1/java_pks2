d79c05c88ec15fb7ab162191517ba1dbed70905a
KOREAVC <KOREAVC@DESKTOP-8341A0F>
2023-09-05 10:00:44 +0900

    8월30일 수업

---
+++ p0830/src/p0830/JA0830_02.java
@@ -0,0 +1,29 @@
+package p0830;
+
+public class JA0830_02 {
+//전체선택 ctrl+a 자동정렬 ctrl+shift+f 행이동 alt+화살표키
+	public static void main(String[] args) {
+		boolean bo = true;
+		boolean bo2 = false;
+		char ch = 'a';
+		char ch2 = 'b';
+		byte by = 1;
+		short sh = 2;
+		int a = 1;
+		int b = 20;
+		int in = 2100000000;
+		long lo = 10000000000L; // 주석 long 에는 값을 집어넣어준 뒤에 L(l)을 붙여야함
+		float fl = 1.2F; // 주석 float 에는 값을 집어넣어준 뒤에 F(f)를 붙여야함
+		double d = 11.12;
+		double dou = 1.2;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+
+	}
+
+}
