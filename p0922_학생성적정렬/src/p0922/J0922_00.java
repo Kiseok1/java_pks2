7f722caf375a350d62b97a712089b6534c20d963
KOREAVC <KOREAVC@DESKTOP-8341A0F>
2023-09-22 19:07:29 +0900

    9월 22일 학생성적입력, 객체,list주고받기

---
+++ p0922_학생성적정렬/src/p0922/J0922_00.java
@@ -0,0 +1,36 @@
+package p0922;
+
+public class J0922_00 {
+
+	public static void main(String[] args) {
+		// --1--
+		// ppt ch12 지네릭스 , enum
+		// 01 (지네릭스 실습)
+		// 02 (enum)
+		
+		// --2--
+		// 02, Week (enum)
+		// 03 JAction 주고받기(int,배열,list컬렉션프레임웍)
+		
+		// --3--
+		// 03 JAction 주고받기 (map컬렉션프레임웍) ★
+		
+		// --4--
+		// StuMain StuAction StuScore (1,2,8)★
+		
+		// --5--
+		// 오라클18c 이클립스 자바웹 개발 다운로드
+		// debug 사용법
+		// StuMain 9(파일 저장)
+		
+		// --6--
+		// StuMain 5,6,7 정렬
+		
+		// --7--
+		// StuMain 3
+		
+		
+
+	}//main
+
+}//class
---
+++ p0922_학생성적정렬/src/p0922/JAction.java
@@ -0,0 +1,63 @@
+package p0922;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class JAction {
+
+	ArrayList<Car> list = new ArrayList();
+	ArrayList<StuScore> list2 = new ArrayList();
+	HashMap<String, Object> map = new HashMap();
+
+	// 리턴타입 : map컬렉션프레임웍
+	HashMap cal(String[] color, int[] door, String[] name, int[] kor, int[] eng, int[] math) {
+
+		list.add(new Car(color[0], door[0]));
+		list.add(new Car(color[1], door[1]));
+		list.add(new Car(color[2], door[2]));
+		
+		list2.add(new StuScore(name[0],kor[0],eng[0],math[0]));
+		list2.add(new StuScore(name[1],kor[1],eng[1],math[1]));
+		list2.add(new StuScore(name[2],kor[2],eng[2],math[2]));
+
+		map.put("list", list);
+		map.put("list2", list2);
+		
+		return map;
+	}
+
+	// 리턴타입 : list 컬렉션프레임웍
+//	ArrayList cal (String[] color, int[] door) {
+//		for (int i=0;i<color.length;i++) {
+//			list.add(new Car(color[i],door[i]));
+//		}
+//	
+//		return list;
+//	}	
+
+	// 리턴타입 : 객체
+//	Car cal (String color, int door) {
+//		Car c = new Car(color,door);
+//	
+//		return c;
+//	}		
+
+	// 리턴타입 : int배열
+//	int[] cal (int n1, int n2) {
+//		int[] result = new int[4];
+//		result[0] = n1+n2;
+//		result[1] = n1-n2;
+//		result[2] = n1*n2;
+//		result[3] = n1/n2;
+//		
+//		return result;
+//	}	
+
+	// 리턴타입 : int
+//	int cal (int n1, int n2) {
+//		int result = n1*n2;
+//		
+//		return result;
+//	}
+
+}// class
